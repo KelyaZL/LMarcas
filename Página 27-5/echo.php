f1a525bdc9d4ccad8e82b6f52bdc8418c897333c
KelyaZL <56816889+KelyaZL@users.noreply.github.com>
2020-05-12 19:16:55 +0200

    Add files via upload

---
+++ Página 27-5/echo.php
@@ -0,0 +1,36 @@
+<h1>Tu envío</h1><br>
+<h4>Lo que hemos recibido</h4>
+
+<?php
+
+echo "<br>";
+if (isset($_GET["capturapedia"]))
+    echo $_GET["capturapedia"];
+
+echo "<br>";
+if (isset($_GET["nabos"]))
+    echo $_GET["nabos"];
+
+echo "<br>";
+if (isset($_GET["creaciones"]))
+    echo $_GET["creaciones"];
+
+echo "<br>";
+if (isset($_GET["boton"]))
+    echo $_GET["boton"];
+        
+echo "<br>";
+if (isset($_GET["email"]))
+    echo $_GET["email"];
+
+echo "<br>";
+if (isset($_GET["personajes"]))
+    echo $_GET["personajes"];
+    
+echo "<br>";
+if (isset($_GET["comentario"]))
+    echo $_GET["comentario"];
+
+echo "<br>";
+if (isset($_GET["jugar"]))
+    echo $_GET["jugar"];
